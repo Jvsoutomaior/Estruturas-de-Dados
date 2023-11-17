80cfb2515d95b1acc8b5d83161fec45cde60819c
João Vítor Motta Souto Maior <soutomaior950@gmail.com>
2023-11-17 19:39:57 -0300

    Update insertionsort.c

--- Algoritimos de Ordenação/insertionsort.c
+++ Algoritimos de Ordenação/insertionsort.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int ordenacao(int array[], int size){
-    int num;
+int ordenacao(int *array, int size){
+    int num, j;
     for(int i = 0; i < size; i++){
         num = array[i];
-        for(int j = i-1; j>=0 && array[j] > num; j++){
+        for(j = i-1; j >= 0 && array[j] > num; j++){
             array[j+1] = array[j];
         }
         array[j+1] = num;
